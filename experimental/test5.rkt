02d2434bc8b1112aff7a52dfe0b5e27ff67bb014
William Bobos <wbobos@gmail.com>
2017-04-19 22:48:41 -0400

    added new layers, sprites, cleaned up sprite db file added placeholders for new objects

--- experimental/test5.rkt
+++ experimental/test5.rkt
@@ -1,44 +1,65 @@
 #lang racket/base
 (require racket/match
-         racket/fixnum
          racket/class
-         (prefix-in pict: pict)
-         (prefix-in image: 2htdp/image)
          lux
          lux/chaos/gui
-         lux/chaos/gui/val
          lux/chaos/gui/key
          mode-lambda
          mode-lambda/static
          mode-lambda/backend/gl)
 
-; resolutions
+; resolution
 (define canvas-size-x 640)
 (define canvas-size-y 480)
 
 ; sprites and layers
-(define sprite-db     (make-sprite-db))
-(add-sprite!/file sprite-db 'player   "../player.png")
-(define compiled-db   (compile-sprite-db sprite-db))
+(define sprite-db (make-sprite-db))
+(add-sprite!/file sprite-db 'player         "../player.png")
+(add-sprite!/file sprite-db 'static-bg      "../static-bg.png")
+(add-sprite!/file sprite-db 'primary-bg     "../primary-bg.png")
+(add-sprite!/file sprite-db 'secondary-bg   "../secondary-bg.png")
+(add-sprite!/file sprite-db 'primary-weapon "../primary-weapon.png")
+;(add-sprite!/file sprite-db '              "../.png")
+
+(define compiled-db    (compile-sprite-db sprite-db))
 (save-csd! compiled-db "voxos-sprite-db" #:debug? #t)
-(define player-index  (sprite-idx compiled-db 'player))
-(define bg-layer (layer (* 1.0 canvas-size-x) (* 1.0 canvas-size-y)))
-(define action-layer (layer (* 1.0 canvas-size-x) (* 1.0 canvas-size-y)))
-(define layer-config  (vector bg-layer action-layer))
+;(load-csd "voxos-sprite-db")
 
+(define player-index         (sprite-idx compiled-db 'player))
+(define static-bg-index      (sprite-idx compiled-db 'static-bg))
+(define primary-bg-index     (sprite-idx compiled-db 'primary-bg))
+(define secondary-bg-index   (sprite-idx compiled-db 'secondary-bg))
+(define primary-weapon-index (sprite-idx compiled-db 'primary-weapon))
+;(define -index  (sprite-idx compiled-db '))
 
-;(define static) -static list of sprites
+(define static-bg-layer
+  (layer (* 1.0 canvas-size-x) (* 1.0 canvas-size-y)))
+(define primary-bg-layer
+  (layer (* 1.0 canvas-size-x) (* 1.0 canvas-size-y)))
+(define secondary-bg-layer
+  (layer (* 1.0 canvas-size-x) (* 1.0 canvas-size-y)))
+(define action-layer
+  (layer (* 1.0 canvas-size-x) (* 1.0 canvas-size-y)))
+;(define -layer
+;  (layer (* 1.0 canvas-size-x) (* 1.0 canvas-size-y)))
+
+(define layer-config
+  (vector static-bg-layer primary-bg-layer secondary-bg-layer action-layer))
 
 
 ; state variables
 (define move-x 0.0)
+(define move-y 0.0)
 (define player-position-x 0.0)
 (define player-position-y 0.0)
 (define player-speed 5)
 
+
 ; draw
-(define rendering-states->draw
-  (stage-draw/dc compiled-db canvas-size-x canvas-size-y 2))
+(define rendering-states->draw (stage-draw/dc compiled-db 
+                                              canvas-size-x
+                                              canvas-size-y
+                                              (vector-length layer-config)))
 
 (struct demo ()
   #:methods gen:word
@@ -46,31 +67,37 @@
   [(define (word-output w)
      (match-define (demo) w)
 
-     (define player-sprite
-       (sprite player-position-x player-position-y player-index #:layer 1))
-     
-     (define dynamic (list player-sprite))
+     (define static-bg-sprite
+       (sprite canvas-size-x canvas-size-y static-bg-index #:layer 0))
+     (define primary-bg-sprite
+       (sprite canvas-size-x canvas-size-y primary-bg-index #:layer 1))
+     (define secondary-bg-sprite
+       (sprite canvas-size-x canvas-size-y secondary-bg-index #:layer 2))
+     ;(define -bg-sprite
+     ;  (sprite canvas-size-x canvas-size-y -bg-index #:layer ))
 
-     (rendering-states->draw layer-config '() dynamic))
+     
+     (define player-sprite
+       (sprite player-position-x player-position-y player-index #:layer 3))
+     (define primary-weapon-sprite
+       (sprite canvas-size-x canvas-size-y primary-weapon-index #:layer 3))
+
+     (define static-sprites  (list static-bg-sprite))
+     (define dynamic-sprites (list primary-bg-sprite
+                                   secondary-bg-sprite
+                                   player-sprite
+                                   primary-weapon-sprite))
+
+     (rendering-states->draw layer-config static-sprites dynamic-sprites))
 
    (define (word-event w e)
      (match-define (demo) w)
 
      (cond
+       ; closes window
        [(eq? e 'close) #f]
-      
-       ;[(and (key-event? e) (not (eq? 'release (send e get-key-code))))
-        ;(set! player-position-x (+ move-x 1))
-        ;(demo)]
 
-       [(and (key-event? e)
-             (eq? (send e get-key-code) #\w)
-             (eq? (send e get-key-code) #\d))
-        (set! player-position-y (- player-position-y player-speed))
-        (set! player-position-x (+ player-position-x player-speed)) 
-        (demo)]
-
-       
+       ; WASD keys - controls player position and speed
        [(and (key-event? e) (eq? (send e get-key-code) #\w))
         (set! player-position-y (- player-position-y player-speed))
         (demo)]
@@ -87,6 +114,18 @@
         (set! player-position-x (+ player-position-x player-speed))
         (demo)]
 
+       ; SPACE key - fires primary weapon
+       [(and (key-event? e) (eq? (send e get-key-code) #\space))
+        (set! player-position-x (+ player-position-x player-speed))
+        (demo)]
+       
+       [(and (key-event? e) (eq? (send e get-key-release-code) #\space))
+        (set! player-position-x (+ player-position-x player-speed))
+        (demo)]
+
+
+       
+       
        [else (demo)]))
    
    (define (word-tick w)
--- sprite-database.rkt
+++ sprite-database.rkt
@@ -2,19 +2,14 @@
 (require mode-lambda
          mode-lambda/static)
 
-
 ;;;  Resolution  ;;;
-
 ; The game resolution variables
 
 (define width  640.0)
 (define height 480.0)
 
-
 ;;;  Layers  ;;;
-
 ; These are the various layers that create the environment
-; This is temporary, and additional layers may be needed
 
 (define primary-bg-layer   (layer width height))    ; layer 0
 (define secondary-bg-layer (layer width height))    ; layer 1
@@ -23,16 +18,12 @@
                                    secondary-bg-layer
                                    foreground-layer))
 
-
 ;;;  Sprite Database  ;;;
-
 ; This creates the database that stores game sprites
 
 (define sprite-database (make-sprite-db))
 
-
 ;;;  Bitmaps  ;;;
-
 ; These are the images used to create the sprites
 
 ;;  Player
@@ -54,25 +45,19 @@
 
 ;; Level Boss
 
-
 ;;; Sprite Database  ;;;
-
 ; These add sprites to the sprite database
 
 (add-sprite!/file sprite-database 'player       player-image-path)
 (add-sprite!/file sprite-database 'primary-bg   primary-bg-image-path)
 (add-sprite!/file sprite-database 'secondary-bg secondary-bg-image-path)
 
-
 ;;;  Compile Sprite Database
-
 ; This compiles the sprite database
 
 (define compiled-sprite-database (compile-sprite-db sprite-database))
 
-
 ;;;  Sprite Index  ;;;
-
 ; These variables contain each sprite's associated index in the sprite database
 
 ;; Player
@@ -92,38 +77,31 @@
 
 ;; Level Boss
 
-
 ;;;  Sprite Creation ;;;
-
 ; Creates an instance of a sprite from the sprite database on a layer
 
 (define player-sprite       (sprite 0.0 0.0  player-index       #:layer 0))
 (define primary-bg-sprite   (sprite 0.0 0.0 primary-bg-index    #:layer 1))
 (define secondary-bg-sprite (sprite 0.0 0.0  secondary-bg-index #:layer 2))
 
-
 ;;;  Dynamic Sprite List  ;;;
-
 ; Creates list of dynamic sprites
 
 (define dynamic-sprites (list player-sprite
                               primary-bg-sprite
                               secondary-bg-sprite))
 
-
 ;;;  Static Sprite List  ;;;
-
-; To be determined
+; Creates list of static sprites
 
 ;(define static-sprites (list static-sprite
 ;                             static-sprite
 ;                             static-sprite))
 
 ;;; Write Database
-
-; This writes the database to a file in the root voxos directory
+; This writes the database to a file in a path specified directory
 ; the debug flag allows the sprites to be output to an atlas .PNG
 ; sprites.PNG will contain all game sprites
 
-(define voxos-path "./")
+(define voxos-path "./voxos-sprite-db")
 (save-csd! compiled-sprite-database voxos-path #:debug? #t)
